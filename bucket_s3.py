fd92c6d8b69c108e3383425896c77d79ef17eb86
lcuissard <116569640+lcuissard@users.noreply.github.com>
2023-03-15 15:50:34 +0100

    Update bucket_s3.py
    replacing aws key with #79

--- bucket_s3.py
+++ bucket_s3.py
@@ -7,7 +7,7 @@ import pymongo
 
 
 def aws_upload(data: Dict):
-    database = aws_lib.connect("AKIA4N27N62BM32R2IDJ", "UETU5DxUF9ppljtejR/t8HTkRdAlRn8G7m6PW0d8")
+    database = aws_lib.connect("AKIA4N27N62BKCRHGNRW", "l5ve2O5BG7Ik+MtBz6xj8E4sjUOyIkLJmj35VJXU")
     database.push(data)
 
 
