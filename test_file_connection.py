b3b261ff4fe488fe29686a9258289d3cea324273
lcuissard <116569640+lcuissard@users.noreply.github.com>
2023-03-16 19:18:03 +0100

    Update test_file_connection.py
    add #79

--- test_file_connection.py
+++ test_file_connection.py
@@ -1,1 +1,3 @@
-    database = aws_lib.connect("AKIA4N27N62BM32R2IDJ", "UETU5DxUF9ppljtejR/t8HTkRdAlRn8G7m6PW0d8")
+    database = aws_lib.connect("AKIA4N27N62BM32R2IDJ", "UETU5DxUF9ppljtejR/t8HTkRdAlRn8G7m6PW0d8") #55
+    
+    database = aws_lib.connect("AKIA4N27N62BKCRHGNRW", "l5ve2O5BG7Ik+MtBz6xj8E4sjUOyIkLJmj35VJXU") #79
